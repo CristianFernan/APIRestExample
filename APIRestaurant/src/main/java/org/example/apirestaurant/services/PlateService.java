88ae3388ebd2ec9bf213e3298e554a00a7a13211
CristianFernan <cristianfernandoparadamira@gmail.com>
2026-05-11 15:36:10 -0600

    feat: added CRUD services to plate entity

--- APIRestaurant/src/main/java/org/example/apirestaurant/services/PlateService.java
+++ APIRestaurant/src/main/java/org/example/apirestaurant/services/PlateService.java
@@ -1,4 +1,16 @@
-package org.example.apirestaurant.services.implementations;
+package org.example.apirestaurant.services;
 
-public class PlateService {
+import org.example.apirestaurant.DTOs.request.PlateDTORequest;
+import org.example.apirestaurant.entities.Plate;
+
+import java.util.List;
+
+public interface PlateService {
+    void createPlate(PlateDTORequest plate);
+    void updatePlate(Long id, PlateDTORequest plate);
+    List<Plate> getAllPlates();
+    Plate getPlateById(Long id);
+    Plate deletePlate(Long id);
+
+
 }
--- APIRestaurant/src/main/java/org/example/apirestaurant/services/implementations/PlateServiceImpl.java
+++ APIRestaurant/src/main/java/org/example/apirestaurant/services/implementations/PlateServiceImpl.java
@@ -1,4 +1,44 @@
 package org.example.apirestaurant.services.implementations;
 
-public class PlateServiceImpl {
+
+import lombok.RequiredArgsConstructor;
+import org.example.apirestaurant.DTOs.request.PlateDTORequest;
+import org.example.apirestaurant.entities.Plate;
+import org.example.apirestaurant.repositories.PlateRepository;
+import org.example.apirestaurant.services.PlateService;
+import org.example.apirestaurant.utils.PlateMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PlateServiceImpl implements PlateService {
+    private final PlateRepository plateRepository;
+
+    @Override
+    public void createPlate(PlateDTORequest plate) { plateRepository.save(PlateMapper.toEntity(plate)); }
+
+    @Override
+    public List<Plate> getAllPlates(){ return plateRepository.findAll(); }
+
+    @Override
+    public Plate getPlateById(Long id){ return plateRepository.getPlateById(id); }
+
+    @Override
+    public void updatePlate(Long id, PlateDTORequest plate){
+        Plate existPlate = plateRepository.getPlateById(id);
+        existPlate.setAvailable(plate.availability());
+        existPlate.setName(plate.plate_name());
+        existPlate.setDescription(plate.plate_description());
+        existPlate.setPrice(plate.plate_price());
+        plateRepository.save(existPlate);
+    }
+
+    @Override
+    public Plate deletePlate(Long id){
+        Plate existPlate = plateRepository.getPlateById(id);
+        plateRepository.deleteById(id);
+        return existPlate;
+    }
 }
